f8f26a7e9563541648a68a0b609bf7c7e418400b
Legellon <52162174+Legellon@users.noreply.github.com>
2020-11-23 12:44:18 +0200

    v1.1

--- index.php
+++ index.php
@@ -1,15 +1,62 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html lang="en" dir="ltr">
+<html lang="en">
+
 <head>
   <meta charset="utf-8">
-  <title></title>
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Presento Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <?php include "moduls/css.php"; ?>
+
 </head>
+
 <body>
 
+  <?php
+  include "moduls/header.php";
+  include "moduls/hero.php";
+  ?>
 
-  <script language="javascript" src="js/jquery-3.5.1.slim.min.js"></script>
-  <script language="javascript" src="js/popper.min.js"></script>
-  <script language="javascript" src="js/bootstrap.min.js"></script>
+  <main id="main">
+
+    <?php
+    include "moduls/clients.php";
+    include "moduls/about.php";
+    include "moduls/counts.php";
+    include "moduls/tabs.php";
+    include "moduls/services.php";
+    include "moduls/portfolio.php";
+    include "moduls/testimonials.php";
+    include "moduls/pricing.php";
+    include "moduls/FAQ.php";
+    include "moduls/teamsection.php";
+    include "moduls/contactsection.php";
+    ?>
+
+  </main><!-- End #main -->
+
+  <?php include "moduls/footer.php"; ?>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
 </body>
+
 </html>
