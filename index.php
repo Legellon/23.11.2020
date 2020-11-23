22295a364f233ddf2c7b442b2e25ab07ea541990
Legellon <52162174+Legellon@users.noreply.github.com>
2020-11-23 10:35:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html lang="en" dir="ltr">
 <head>
   <meta charset="utf-8">
@@ -6,7 +6,7 @@
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
-  
+
 
   <script language="javascript" src="js/jquery-3.5.1.slim.min.js"></script>
   <script language="javascript" src="js/popper.min.js"></script>
